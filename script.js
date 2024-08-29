d35051f1bbcaa2b754c84d8def616e32076d4106
w13nu <wisnuardiansyah94370@gmail.com>
2024-08-29 14:09:11 +0700

    commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Dark Mode Toggle (Opsional)
+document.querySelector(".dark-mode-toggle").addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
+});
+
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
